0055ffb15fabe1644e758ee34563d306a3dbc7b8
VIVPM <vivpm99@gmail.com>
2026-03-04 12:55:33 +0530

    added files

---
+++ main.py
@@ -0,0 +1,5 @@
+from books_recommender.pipeline.training_pipeline import TrainingPipeline
+
+if __name__ == "__main__":
+    training_pipeline = TrainingPipeline()
+    training_pipeline.start_training_pipeline()
