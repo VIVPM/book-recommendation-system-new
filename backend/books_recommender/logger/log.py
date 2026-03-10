230c66fc4f32dc2d1e478c4ffadc1aaa3dd1f5e7
VIVPM <vivpm99@gmail.com>
2026-03-10 11:51:18 +0530

    newww3

--- backend/books_recommender/logger/log.py
+++ backend/books_recommender/logger/log.py
@@ -18,7 +18,12 @@ file_name = f"log_{CURRENT_TIME_STAMP}.log"
 log_file_path = os.path.join(LOG_DIR, file_name)
 
 
-logging.basicConfig(filename=log_file_path,
-                    filemode='w',
-                    format='[%(asctime)s] %(name)s - %(levelname)s - %(message)s',
-                    level=logging.NOTSET)
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s] %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler(log_file_path, mode='w'),
+        logging.StreamHandler()
+    ],
+    force=True
+)
